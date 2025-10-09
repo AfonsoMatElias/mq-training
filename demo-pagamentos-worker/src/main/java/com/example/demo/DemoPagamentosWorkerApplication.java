d523b38cec19a6768ad8270af13550b97874dc2c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-09 17:08:39 -0300

    initial

---
+++ demo-pagamentos-worker/src/main/java/com/example/demo/DemoPagamentosWorkerApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableRabbit
+@SpringBootApplication
+public class DemoPagamentosWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPagamentosWorkerApplication.class, args);
+		System.out.println("Rodando Worker");
+	}
+
+}
